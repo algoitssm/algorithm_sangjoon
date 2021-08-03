59a9d4f566f05cdfdb719b3b21c65ea8e388ea83
onshore <onshorecoding9@gmail.com>
2021-08-03 22:49:36 +0900

    210803: swea d2

---
+++ Swea/1859_d2_백만 장자 프로젝트.py
@@ -0,0 +1,20 @@
+from re import M
+
+
+test = int(input())
+
+for i in range(1, test + 1):
+    n = int(input())
+    lst = list(map(int, input().split()))
+    lst.reverse()
+
+    ans = 0
+    max_price = 0
+
+    for price in lst:
+        if price > max_price:
+            max_price = price
+        else:
+            ans += max_price - price
+
+    print(f"#{i} {ans}")
