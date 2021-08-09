d161dc0c54dcf0041a0b78891c7ece63befb6322
onshore <onshorecoding9@gmail.com>
2021-08-09 21:24:10 +0900

    210809: algorithm baekjoon dp s5

--- Baekjoon/14916_DP_거스름돈.py
+++ Baekjoon/14916_DP_거스름돈.py
@@ -1,11 +1,18 @@
+def check_change(n: int):
+    if n in [1, 3]:
+        return -1
+
+    five, mod = divmod(n, 5)
+
+    if mod % 2:
+        five -= 1
+        two = (mod + 5) // 2
+    else:
+        two = mod // 2
+
+    return five + two
+
+
 n = int(input())
-
-five, mod = divmod(n, 5)
-
-if mod % 2:
-    five -= 1
-    two = (mod + 5) // 2
-    print(five + two)
-else:
-    two = mod // 2
-    print(five + two)
+ans = check_change(n)
+print(ans)
