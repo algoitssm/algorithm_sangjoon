8fae1bea52d71f8609df5103a3c6001ef19f26cf
onshore <onshorecoding9@gmail.com>
2021-08-12 22:04:38 +0900

    210811: algorithm baekjoon s4 greedy

--- Baekjoon/13305_그리디_주유소.py
+++ Baekjoon/13305_그리디_주유소.py
@@ -2,6 +2,16 @@ n = int(input())
 path = list(map(int, input().split()))
 charge = list(map(int, input().split()))
 
+ans, pay = 0, charge[0]
+
+for i in range(n - 1):
+    if charge[i] < pay:
+        pay = charge[i]
+    ans += pay * path[i]
+
+print(ans)
+
+
 # charge = charge[:-1]
 # ans, idx = 0,n
 
@@ -11,12 +21,3 @@ charge = list(map(int, input().split()))
 
 #     ans += min_c * sum(path[idx:])
 #     path, charge = path[:idx], charge[:idx]
-
-ans, pay = 0, charge[0]
-
-for i in range(n - 1):
-    if charge[i] < pay:
-        pay = charge[i]
-    ans += pay * path[i]
-
-print(ans)
