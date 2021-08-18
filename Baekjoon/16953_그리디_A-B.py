61bd4fdfa72e9152ed48e364db0c9295850766f9
onshore <onshorecoding9@gmail.com>
2021-08-18 21:25:47 +0900

    210818: algorithm baekjoon s1 greedy

---
+++ Baekjoon/16953_그리디_A-B.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+
+def count_a_to_b(a, b):
+    dq = deque([(a, 1)])
+
+    while dq:
+        e, cnt = dq.popleft()
+        if e == b:
+            return cnt
+
+        if e > b:
+            continue
+
+        dq.append((e * 2, cnt + 1))
+        dq.append((e * 10 + 1, cnt + 1))
+
+    return -1
+
+
+a, b = map(int, input().split())
+ans = count_a_to_b(a, b)
+print(ans)
