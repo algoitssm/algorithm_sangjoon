75f5714a8923cd0c17f50709372b6d04fb435154
onshore <onshorecoding9@gmail.com>
2021-09-05 10:51:49 +0900

    210905: algorithm baekjoon g5

---
+++ Baekjoon/17836_BFS_공주님을 구해라.py
@@ -0,0 +1,45 @@
+from collections import deque
+from pprint import pprint
+
+
+def bfs(x, y):
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+
+    dq = deque([(x, y, 0)])
+    mp[x][y] = 1
+
+    while dq:
+        x, y, cnt = dq.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if nx == n - 1 and ny == m - 1:
+                    ans.append(cnt + 1)
+
+                if mp[nx][ny] == 0 and cnt < t - 1:
+                    mp[nx][ny] = 1
+                    dq.append((nx, ny, cnt + 1))
+
+                if mp[nx][ny] == 2 and cnt < t - 1:
+                    mp[nx][ny] = 1
+                    cnt += (n - 1 - nx) + (m - 1 - ny) + 1
+                    if cnt <= t:
+                        ans.append(cnt)
+
+    return
+
+
+n, m, t = map(int, input().split())
+mp = [list(map(int, input().split())) for _ in range(n)]
+
+ans = []
+bfs(0, 0)
+
+if ans:
+    print(min(ans))
+else:
+    print("Fail")
