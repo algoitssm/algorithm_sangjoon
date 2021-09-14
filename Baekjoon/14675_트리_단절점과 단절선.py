cfeeb178675e7ea9a18af979d6d72ace67bcb248
onshore <onshorecoding9@gmail.com>
2021-09-14 23:10:37 +0900

    210914: algorithm baekjoon g5

---
+++ Baekjoon/14675_트리_단절점과 단절선.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+
+input = stdin.readline
+
+n = int(input())
+tr = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    tr[a].append(b)
+    tr[b].append(a)
+
+q = int(input())
+
+for _ in range(q):
+    t, k = map(int, input().split())
+    if t == 1 and len(tr[k]) == 1:
+        print("no")
+    else:
+        print("yes")
