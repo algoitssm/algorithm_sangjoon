3716af19092b94e4d407113955f8e959e7b70475
onshore <onshorecoding9@gmail.com>
2021-09-24 15:16:16 +0900

    210934: algorithm swea

---
+++ Swea/1952.py
@@ -0,0 +1,24 @@
+test = int(input())
+
+
+def dfs(n, cost):
+    global ans
+    if n > 11:
+        ans = min(ans, cost)
+        return
+
+    if schedule[n]:
+        dfs(n+1, cost + min(schedule[n]*d, m1))
+        dfs(n+3, cost + m3)
+
+    else:
+        dfs(n+1, cost)
+
+
+for test_case in range(1, test+1):
+    d, m1, m3, y = map(int, input().split())
+    schedule = list(map(int, input().split()))
+    ans = y
+
+    dfs(0, 0)
+    print(f"#{test_case} {ans}")
