d51b5a7428ac7b83e221ffc405f1d24d8274f2d7
onshore <onshorecoding9@gmail.com>
2021-09-25 21:56:20 +0900

    210925: algorithm swea

---
+++ Swea/10966.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+test = int(input())
+
+for test_case in range(1, test+1):
+    n, m = map(int, input().split())
+    gh = [list(input()) for _ in range(n)]
+    visited = [[-1] * m for _ in range(n)]
+    dx = [0, 1, 0, -1]
+    dy = [-1, 0, 1, 0]
+    dq = deque([])
+
+    ans = 0
+
+    # 시작지점 표시
+    for i in range(n):
+        for j in range(m):
+            if gh[i][j] == "W":
+                visited[i][j] = 0
+                dq.append((i, j))
+
+    # bfs 탐색
+    while dq:
+        x, y = dq.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == -1:
+                visited[nx][ny] = visited[x][y] + 1
+                ans += visited[x][y] + 1
+                dq.append((nx, ny))
+
+    print(f"#{test_case} {ans}")
