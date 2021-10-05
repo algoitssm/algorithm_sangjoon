6b92808876e928a9d70a56baad527579524e4da9
onshore <onshorecoding9@gmail.com>
2021-10-05 17:51:08 +0900

    211005: algorithm baekjoon s1

---
+++ Baekjoon/18222.py
@@ -0,0 +1,13 @@
+# 반씩 접는 방식을 떠올림
+def thue_morse(k):
+    cnt = 0
+    while k:
+        i = len(format(k, "b")) - 1
+        k = k - 2**i
+        cnt += 1
+
+    return 1 if cnt % 2 else 0
+
+
+k = int(input())
+print(thue_morse(k-1))
