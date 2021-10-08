c0dcc67d375c441561660d3546f7bab38f8307f1
onshore <onshorecoding9@gmail.com>
2021-10-08 21:03:47 +0900

    211008: algorithm baekjoon s3

---
+++ Baekjoon/15650.py
@@ -0,0 +1,11 @@
+def combination(idx, l):
+
+    if len(l) == c:
+        print(" ".join(l))
+        return
+
+    for i in range(idx, n+1):
+        combination(idx+1, l+[str(i)])
+
+
+n, c = map(int, input().split())
