8f7cd209ac25ca96798f275b0acc1ef3a39af4a2
onshore <onshorecoding9@gmail.com>
2021-10-10 01:39:27 +0900

    211010: algorithm baekjoon s3

---
+++ Baekjoon/15652.py
@@ -0,0 +1,10 @@
+def dfs(s, m, lst):
+    if m == 0:
+        print(" ".join(map(str, lst)))
+        return
+    for i in range(s, n+1):
+        dfs(i, m-1, lst+[i])
+
+
+n, m = map(int, input().split())
+dfs(1, m, [])
