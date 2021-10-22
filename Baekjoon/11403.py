f1b2a93a5ac1a2b928b987b782b55de57ad23be8
onshore <onshorecoding9@gmail.com>
2021-10-22 22:42:53 +0900

    211022: algorithm baekjoon s1

---
+++ Baekjoon/11403.py
@@ -0,0 +1,19 @@
+# 플로이드 와샬 알고리즘
+
+n = int(input())
+mp = [[0]*n for _ in range(n)]
+
+for i in range(n):
+    line = list(map(int, input().split()))
+    for j in range(n):
+        if line[j]:
+            mp[i][j] = 1
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if mp[i][k] and mp[k][j]:
+                mp[i][j] = 1
+
+for l in mp:
+    print(*l)
