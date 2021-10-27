04da20ccd0768ce1bf41d3ab6dbf049219553c9f
onshore <onshorecoding9@gmail.com>
2021-10-27 21:51:50 +0900

    211017: algorithm swea

--- Swea/5648.py
+++ Swea/5648.py
@@ -7,26 +7,28 @@ def check_collision(a, b):
     xa, ya, da, ka = a
     xb, yb, db, kb = b
 
-    if (xa-xb) == 0 or (ya-yb) == 0:
+    if (xa-xb) == 0 or (ya-yb) == 0:  # 직선에 위치 할 경우
         if ya > yb:
-            if da != 1 or db != 0:
-                return 0, 0
+            if da == 1 and db == 0:
+                return 1, round((abs(xa-xb) + abs(ya-yb))/2, 2)
 
         if ya < yb:
-            if da != 0 or db != 1:
-                return 0, 0
+            if da == 0 and db == 1:
+                return 1, round((abs(xa-xb) + abs(ya-yb))/2, 2)
 
         if xa > xb:
-            if da != 2 or db != 3:
-                return 0, 0
+            if da == 2 and db == 3:
+                return 1, round((abs(xa-xb) + abs(ya-yb))/2, 2)
 
         if xa < xb:
-            if da != 3 or db != 2:
-                return 0, 0
+            if da == 3 and db == 2:
+                return 1, round((abs(xa-xb) + abs(ya-yb))/2, 2)
 
+    # 시간상 접점이 생기지 않는 경우
     if abs(xa-xb) - abs(ya-yb):
         return 0, 0
 
+    # 방향상 접점이 생기지 않는 경우
     if (xa > xb and ya > yb):
         if (da, db) not in [(1, 3), (2, 0)]:
             return 0, 0
@@ -43,7 +45,7 @@ def check_collision(a, b):
         if (da, db) not in [(3, 1), (0, 2)]:
             return 0, 0
 
-    return 1, round((abs(xa-xb) + abs(ya-yb))/2, 1)
+    return 1, round((abs(xa-xb) + abs(ya-yb))/2, 2)
 
 
 ans = []
@@ -55,23 +57,27 @@ for test in range(1, test_case + 1):
     collisions = defaultdict(list)
     visited = set()
     res = 0
+
+    # 충돌 여부 확인
     for a, b in combinations(elements, 2):
         chk, t = check_collision(a, b)
         if chk:
-            print(a, b)
             collisions[t].append((a, b))
+
+    # 충돌 시간 순 배열
     collisions = list(sorted(collisions.items()))
-    # print(collisions)
 
     for t, eles in collisions:
         tv = set()
+
+        # 시간대별 충돌 순회(이전에 충돌 여부 확인)
         for ele in eles:
             a, b = ele
             if a not in visited and b not in visited:
                 tv.add(a)
                 tv.add(b)
-        if tv:
-            print(t, tv)
+
+        # 방출에너지 계산
         for e in tv:
             visited.add(e)
             res += e[3]
