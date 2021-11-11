fb2a4a4095b37381ab954869605470beb20ff1e8
onshore <onshorecoding9@gmail.com>
2021-11-11 21:26:05 +0900

    211111: algorithm programmers l2

---
+++ Programmers/12899.py
@@ -0,0 +1,13 @@
+def to_elice(n):
+    lst = ["1","2","4"]
+    
+    if n <= 3:
+        return lst[n-1]
+    
+    else:  
+        d, m = divmod(n-1, 3)
+        return solution(d) + lst[m] 
+
+def solution(n):
+    answer = to_elice(n)
+    return answer
