6fe49e97b87b81052b26cacd0519ac63306ff997
onshore <onshorecoding9@gmail.com>
2021-12-15 14:31:20 +0900

    211215: programmers fix code

--- Programmers/62048.py
+++ Programmers/62048.py
@@ -8,12 +8,7 @@ def get_gcd(x, y):
 def solution(w, h):
     answer = 1
     gcd = get_gcd(w, h)
-    # 최대 공약수가 1인 경우
-    if gcd == 1:
-        answer = w*h - (w + h - 1)
-    # 최대 공약수가 1이 아닌 경우
-    else:
-        answer = w*h - (w + h - gcd)
+    answer = w*h - (w + h - gcd)
     return answer
 
 
