99a6b99fd891a0b79b21478ab7b0b2431da1b5d5
onshore <onshorecoding9@gmail.com>
2022-04-01 17:28:52 +0900

    220401: algoritm programmers

---
+++ Programmers/42839.py
@@ -0,0 +1,30 @@
+import math
+from itertools import permutations
+
+
+def check(n):
+    if n==0 or n==1:                               
+        return False
+    else:
+        for i in range(2, int(math.sqrt(n)) + 1):   
+            if n % i == 0:                         
+                return False
+        return True  
+
+def solution(numbers):
+    answer = 0
+
+    lst = set()
+    
+    for i in range(len(numbers)):
+        for perm in permutations(numbers, i+1):
+            num = int("".join(perm))
+            if num not in lst:
+                if check(num):
+                    lst.add(num)
+                    answer += 1
+    
+    return answer
+
+
+print(solution("011"))
